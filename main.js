85dcca9cb63cf143be0e50c23739f5cb778cdc05
zikaleste09 <115758432+zikaleste09@users.noreply.github.com>
2022-10-27 21:57:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+images = ["https://www.bing.com/ck/a?!&&p=1ed99fc7c79bbfffJmltdHM9MTY2NjgyODgwMCZpZ3VpZD0xMWFkZGU3OC05YmViLTYzZTItMDk4OS1jYzc3OWE4MDYyNGImaW5zaWQ9NTEzMw&ptn=3&hsh=3&fclid=11adde78-9beb-63e2-0989-cc779a80624b&u=a1aHR0cHM6Ly9oYW1idXJndWVyZGVzdWNlc3NvLmNvbS5ici9tZWxob3ItYmxlbmQtZGUtY2FybmUtcGFyYS1oYW1idXJndWVyLWNvbW8tcHJlcGFyYXIv&ntb=1", "https://th.bing.com/th/id/R.88a927aef0c604beb177ebe44f3707a5?rik=EkHKw2S9tY7J6g&riu=http%3a%2f%2f4.bp.blogspot.com%2f-Hjd2D32yiyI%2fVpF8TVvld-I%2fAAAAAAAAAN4%2fcq3vIUUWogA%2fs1600%2fsushi_0.jpg&ehk=pmscJqaDkMZjMiTWiSFhTT2WWmIGhoVvzOyRXNpQ5gE%3d&risl=&pid=ImgRaw&r=0", "https://i0.wp.com/www.essemundoenosso.com.br/wp-content/uploads/2020/07/comidas-sao-paulo-2-virado-a-paulista.jpg?resize=740%2C493&ssl=1", "https://th.bing.com/th/id/R.187b1136e364623713fac7e48072358b?rik=duUaK4NGqit%2fnw&pid=ImgRaw&r=0"];
+var i = 0;
+function proximoslide() {
+    if (i == 9) { i = 0; } document.getElementById("album").src = images[i];
+    i++
+}
+function SwitchOff(){ document.getElementById('doces').src='https://th.bing.com/th/id/OIP.wwPsY3QfIiHaD2JyH75_ewEyDM?pid=ImgDet&rs=1' } function SwitchOff(){ document.getElementById('doces').src='https://th.bing.com/th/id/OIP.8_Zv9tLOnUxG5pONI0Y3UQHaE7?pid=ImgDet&rs=1' }
+
+
+function On(){ document.getElementById('doce').src='https://www.receitadahora.com/wp-content/uploads/2019/10/torta-morango-confeitaria2.jpg' } function On(){ document.getElementById('doce').src='https://www.frimesa.com.br/upload/image/recipe/frimesa-receita-brownie-de-doce-de-leite2-90-99.jpg' }
+
